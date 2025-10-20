6bf0fb7b019abaaee2114ba6ea56e7f7d41f0398
Gabriel Geronazzo <60116449+ImGabreuw@users.noreply.github.com>
2025-10-20 12:47:12 +0000

    Add requirements.txt and update app and test files for improved functionality

--- src/app.py
+++ src/app.py
@@ -1,14 +1,19 @@
 from wsgiref.simple_server import make_server
 
-import falcon
-from falcon import Request, Response, App
+from falcon import Request, Response, App, HTTP_200, HTTP_201
 
 
 class TestResource:
 
     def on_get(self, req: Request, resp: Response):
         resp.media = {"message": "GET request received"}
-        resp.status = falcon.HTTP_200
+        resp.status = HTTP_200
+        
+    def on_post(self, req: Request, resp: Response):
+        data = req.media
+        
+        resp.media = {"message": "POST request received", "data": data}
+        resp.status = HTTP_201
 
 app = App()
 
--- tests/test_test_resource.py
+++ tests/test_test_resource.py
@@ -6,8 +6,21 @@ def api_address():
     return "http://localhost:8080"
 
 class TestTestResource:
+
+    ENDPOINT = "/test"
+
     def test_get_endpoint(self, api_address):
-        response = requests.get(f"{api_address}/test")
+        response = requests.get(f"{api_address}{self.ENDPOINT}")
 
         assert response.status_code == 200
         assert response.json() == {"message": "GET request received"}
+
+    def test_post_endpoint(self, api_address):
+        payload = {"key": "value"}
+        response = requests.post(f"{api_address}{self.ENDPOINT}", json=payload)
+
+        assert response.status_code == 201
+        assert response.json() == {
+            "message": "POST request received",
+            "data": payload
+        }
