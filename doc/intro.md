35f3f721a6c40f8352fb164b9b567b9b07949674
ImGabreuw <imgabreuw.dev@gmail.com>
2026-01-12 17:16:02 -0300

    chore: initialize project structure with essential files and configurations

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to moneta
+
+TODO: write [great documentation](https://jacobian.org/writing/what-to-write/)
---
+++ test/moneta/core_test.clj
@@ -0,0 +1,7 @@
+(ns moneta.core-test
+  (:require [clojure.test :refer :all]
+            [moneta.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
